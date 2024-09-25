8b2e4bcccab43081e29c10754ac35672e323980b
Alejandro Duhalde <aaduhalde@outlook.es>
2024-09-25 16:42:36 -0300

    update

--- oracle18cEX/demo/plsql_developer18c.sql
+++ oracle18cEX/demo/plsql_developer18c.sql
@@ -1,2 +1,27 @@
-SELECT * FROM dual;
+
+SET timing ON
+SET verify OFF
+SET SERVEROUTPUT ON SIZE 10000
+WHENEVER SQLERROR EXIT ROLLBACK --CONTINUE
+ALTER SESSION SET NLS_DATE_FORMAT='dd/mm/yyyy'
+/
+
+
+
+COLUMN USERNAME FORMAT a30
+COLUMN DEFAULT_TABLESPACE FORMAT a20
+SELECT USERNAME , DEFAULT_TABLESPACE from DBA_USERS
+/
+
+
+SELECT NAME FROM v$database;
+/
+
+
+PROMPT ********************************* * * * * * * Alejandro Adrian Duhalde
+
+
+--Crear usuario en esquema XE
+--Crear entidad para usuario esquema XE
+
 
